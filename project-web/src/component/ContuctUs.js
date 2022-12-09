1390c2156ead41f0bdfe461f35d8b49b355d9512
muddassirali8089 <114619571+muddassirali8089@users.noreply.github.com>
2022-12-09 21:07:47 +0500

    form making

--- project-web/src/component/ContuctUs.js
+++ project-web/src/component/ContuctUs.js
@@ -2,8 +2,41 @@
 import '../CSSS/ContuctUs.css'
 function ContuctUs() {
     return (  
-        <div>
-            <div  id="contuctus" >
+        <div id>
+
+
+<div>
+        <section className="Contact">
+          <h3>
+            Let Contact us
+          </h3>
+        </section>
+        <form 
+      method='POST' 
+      name='contactform' 
+      className='contactForm'>
+
+      <input 
+        type='text' 
+        name='name' 
+        placeholder='Enter your name' />
+
+      <input 
+        type='email' 
+        name='email' 
+        placeholder='Enter your email' />
+
+      <textarea 
+        name='message' 
+        placeholder='Messaage'></textarea>
+
+      <button type='submit' id = "btnn">Submit</button>
+
+    </form>
+
+    
+    </div>
+            {/* <div  id="contuctus" >
   <             div class="card-body" style={{height : "35vh" , marginTop:"13.5vh"}} >
                 <h5 class="card-title"><h1>Contuct Us</h1></h5>
                 <p class="card-text"><h4>PHONE NUMBER +923305375289 </h4></p>
@@ -12,7 +45,7 @@ function ContuctUs() {
                 
                 </div>
   <img style={{height : "200px"}}src={require('../CSSS/contuctusimg.jpg')} class="card-img-bottom" alt="..."/>
-</div>
+</div> */}
             
         </div>
     );
